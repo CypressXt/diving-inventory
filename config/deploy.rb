83a639a93f7aaaeac41fb83d5c2baaf57fb37e43
Clément Hampaï <clement.hampai@cypressxt.net>
2020-08-31 02:48:35 +0200

    Updated deploy to add yarn

--- config/deploy.rb
+++ config/deploy.rb
@@ -39,3 +39,16 @@ append :linked_files, "config/database.yml", "config/credentials.yml.enc", "conf
 # set :ssh_options, verify_host_key: :secure
 
 after 'deploy:updated', 'webpacker:precompile'
+
+before "deploy:assets:precompile", "deploy:yarn_install"
+
+namespace :deploy do
+  desc 'Run rake yarn:install'
+  task :yarn_install do
+    on roles(:web) do
+      within release_path do
+        execute("cd #{release_path} && yarn install")
+      end
+    end
+  end
+end
