db0b18a78974d8e51c34e1fb9a1c9d6c736dc551
Clement Hampai <clement.hampai@cypressxt.net>
2021-08-21 18:13:10 +0200

    Updated db schema

--- db/schema.rb
+++ db/schema.rb
@@ -2,8 +2,8 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# This file is the source Rails uses to define your schema when running `rails
-# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
 # be faster and is potentially less error prone than running all of your
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 2020_09_04_222505) do
     t.datetime "emptying_timestamp"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "user_id"
+    t.bigint "user_id"
     t.index ["tank_id"], name: "index_pressurelogs_on_tank_id"
     t.index ["user_id"], name: "index_pressurelogs_on_user_id"
   end
